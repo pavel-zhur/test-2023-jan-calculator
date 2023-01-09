c9d6bc311bd4a099cf31e81a1a0978dd5595a74c
Pavel Zhur <Pavel.Zhur@GUARDIANTECHNOLOGYGROUP.COM>
2023-01-09 21:57:39 +0300

    minor code cleanup

--- Test.Calculator.Tests/PrintTests.cs
+++ Test.Calculator.Tests/PrintTests.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Test.Calculator.Exceptions;
 using Test.Calculator.Operations;
 using Test.Calculator.Operations.Base;
